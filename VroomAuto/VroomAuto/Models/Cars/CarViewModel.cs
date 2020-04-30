a3cfdf3123eb271211cd9738638898682af72dbf
Dumitru Cristina-Nicoleta <33783647+meow123321@users.noreply.github.com>
2020-05-01 00:29:20 +0300

    cris_0.8

---
+++ VroomAuto/VroomAuto/Models/Cars/CarViewModel.cs
@@ -0,0 +1,13 @@
+﻿using VroomAuto.AppLogic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VroomAuto.Models.Cars
+{
+    public class CarViewModel
+    {
+        public IEnumerable<Car> Cars { get; set; }
+    }
+}
